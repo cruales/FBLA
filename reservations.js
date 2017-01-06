a9e8f4b4467b06dc6bf1841e5c45cbd86d55830f
cruales <christie.ruales@gmail.com>
2017-01-06 00:32:43 -0500

    final

--- reservations.js
+++ reservations.js
@@ -2,7 +2,8 @@ $("#calendar").datepicker(
 {
     onSelect: function()
     { 
-        var dateObject = $(this).datepicker('getDate'); 
-        console.log(dateObject);
+        var date = $(this).datepicker({dateFormat: 'yy-mm-dd'}).val(); 
+        var email = document.getElementById('reservation-email').value
+        document.getElementById('reservation-confirm').innerHTML = 'Reservation for ' + date + ' has been made. A confirmation email will be sent to your email: ' + email + '. Thank you!';
     }
 });
