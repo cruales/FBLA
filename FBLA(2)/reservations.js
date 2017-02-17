cc047c91ee8803c0dee89ce4a8ca6c8dd7754ad8
Paul Ruales <paul-ruales@uiowa.edu>
2017-02-16 18:18:12 -0600

    redo

--- FBLA(2)/reservations.js
+++
@@ -1,9 +0,0 @@
-$("#calendar").datepicker(
-{
-    onSelect: function()
-    { 
-        var date = $(this).datepicker({dateFormat: 'yy-mm-dd'}).val(); 
-        var email = document.getElementById('reservation-email').value
-        document.getElementById('reservation-confirm').innerHTML = 'Reservation for ' + date + ' has been made. A confirmation email will be sent to your email: ' + email + '. Thank you!';
-    }
-});
